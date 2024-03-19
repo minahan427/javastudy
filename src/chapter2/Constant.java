5dfac59821ffc419ebde91412db31aa84f12232d
minahan427 <hma9656@gmail.com>
2024-03-19 17:39:37 +0900

    추가실습파일

---
+++ src/chapter2/Constant.java
@@ -0,0 +1,15 @@
+package chapter2;
+
+public class Constant {
+
+	public static void main(String[] args) {
+		// 상수와 리터럴
+		
+		// 상수 선언 키워드 : final
+		// 상수 이름은 대문자로 정의한다. 규칙은 아니고 관례.
+		// 상수 이름으로 값을 변경할 수 없다.
+		final int MAX_NUM = 100; // final 키워드 제외하면 변수라고 부른다.
+
+	}
+
+}
